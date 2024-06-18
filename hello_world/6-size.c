5c7888701ff0e0c57f8fc4cd0dce23484b7c19bf
siccaa <mahmoudyaya33@gmail.com>
2024-06-18 13:18:04 +0000

    hello

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -13,10 +13,10 @@
 */
 int main(void)
 {
-printf("Size of a char: byte(s)\n", sizeof(char));
-printf("Size of an int: byte(s)\n", sizeof(int));
-printf("Size of a long int: byte(s)\n", sizeof(long int));
-printf("Size of a long long int: byte(s)\n", sizeof(long long int));
-printf("Size of a float: byte(s)\n", sizeof(float))
+printf("Size of a char: 1 byte(s)\n", sizeof(char));
+printf("Size of an int: 4 byte(s)\n", sizeof(int));
+printf("Size of a long int: 4 byte(s)\n", sizeof(long int));
+printf("Size of a long long int: 8 byte(s)\n", sizeof(long long int));
+printf("Size of a float: 4 byte(s)\n", sizeof(float))
 return (0);
 }
