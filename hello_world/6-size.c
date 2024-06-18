83f9372159e673d8052d01b1e8c748ae5f187a89
siccaa <mahmoudyaya33@gmail.com>
2024-06-18 12:13:53 +0000

    size

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
-/**
-*main - Entry point
-*
-*Return: Always 0 (Success)
-*Description - Prints the sizes of various types
+/**                                                                                                                                                                                                              
+*main - Entry point                                                                                                                                                                                              
+*                                                                                                                                                                                                                
+*Return: Always 0 (Success)                                                                                                                                                                                      
+*Description - Prints the sizes of various types                                                                                                                                                                 
 */
 int main(void)
 {
-printf("Size of char: %lu byte(s)\n", sizeof(char));
-printf("Size of short: %lu byte(s)\n", sizeof(short));
-printf("Size of int: %lu byte(s)\n", sizeof(int));
-printf("Size of long: %lu byte(s)\n", sizeof(long));
+printf("Size of a char: %lu byte(s)\n", sizeof(char));
+printf("Size of a short: %lu byte(s)\n", sizeof(short));
+printf("Size of a int: %lu byte(s)\n", sizeof(int));
+printf("Size of a long: %lu byte(s)\n", sizeof(long));
 printf("Size of long long: %lu byte(s)\n", sizeof(long long));
 printf("Size of float: %lu byte(s)\n", sizeof(float));
 printf("Size of double: %lu byte(s)\n", sizeof(double));
 printf("Size of long double: %lu byte(s)\n", sizeof(long double));
 return 0;
 }
+
+
+
+
+
