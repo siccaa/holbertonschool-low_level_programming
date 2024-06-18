5c54e0fd2fecc225ce649cc59b3f014f28d09206
siccaa <mahmoudyaya33@gmail.com>
2024-06-18 12:16:50 +0000

    size

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,24 +1,15 @@
-#include <stdio.h>
-/**                                                                                                                                                                                                              
-*main - Entry point                                                                                                                                                                                              
-*                                                                                                                                                                                                                
-*Return: Always 0 (Success)                                                                                                                                                                                      
-*Description - Prints the sizes of various types                                                                                                                                                                 
-*/
-int main(void)
-{
-printf("Size of a char: %lu byte(s)\n", sizeof(char));
-printf("Size of a short: %lu byte(s)\n", sizeof(short));
-printf("Size of a int: %lu byte(s)\n", sizeof(int));
-printf("Size of a long: %lu byte(s)\n", sizeof(long));
-printf("Size of long long: %lu byte(s)\n", sizeof(long long));
-printf("Size of float: %lu byte(s)\n", sizeof(float));
-printf("Size of double: %lu byte(s)\n", sizeof(double));
-printf("Size of long double: %lu byte(s)\n", sizeof(long double));
-return 0;
-}
-
-
-
-
-
+#include <stdio.h>                                                                                                                  
+                                                                                                                                       
+/**                                                                                                                                 
+* main - Entry point                                                                                                               
+* Return: Always 0 (Success)                                                                                                       
+*/                                                                                                                                 
+int main(void)                                                                                                                      
+{                                                                                                                                   
+      printf("Size of a char: %lu byte(s)\n", sizeof(char));                                                                      
+      printf("Size of an int: %lu byte(s)\n", sizeof(int));                                                                       
+      printf("Size of a long int: %lu byte(s)\n", sizeof(long int));                                                              
+      printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));                                                     
+        printf("Size of a float: %lu byte(s)\n", sizeof(float));                                                                    
+       return (0);                                                                                                                 
+  }
