e3ef685a7b0a0a8bdb5981e1ed100565d4055f98
siccaa <mahmoudyaya33@gmail.com>
2024-06-18 10:17:52 +0000

    printf

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -4,11 +4,12 @@
  * main - Entry point
  * Return: Always 0 (Success)
  * discreption -prints
-
  */
 
 int main(void)
 {
+  
  printf("with proper grammar, but the outcome is a piece of art,\n");
   return (0);
+  
 }
