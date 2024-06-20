2ff910fe05981d615d445c42a2a1e226ac9ca29f
siccaa <mahmoudyaya33@gmail.com>
2024-06-20 13:11:00 +0000

    print

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
+
 /**
+ * main - Entrry point
  *
- * main - Prints all the numbers of base 16 in lowercase 
- * Return: 0;  
- */ 
+ * Return: 0
+ */
 int main(void)
 {
-  int num;
-for(num = 0; num <= 15; num++)
-  putchar("%x , num);
-}
+	char a = 'z';
 
-	  {
-for("n\");
- return(0);
-    }
+	while (a >= 'a')
+	{
+		putchar(a);
+		a--;
+	}
+	putchar('\n');
+	return (0);
+}
