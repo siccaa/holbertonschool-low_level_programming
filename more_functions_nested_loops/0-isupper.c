062569109fe59bf452bd98a05ad310214b2c45a8
siccaa <mahmoudyaya33@gmail.com>
2024-06-25 09:33:39 +0000

    ispper

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+  if (character == character.toUpperCase()) {
+ alert ('upper case true');
+}
+if (character == character.toLowerCase()){
+ alert ('lower case true');
+}
