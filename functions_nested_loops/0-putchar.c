ff58907d6e8eea56b00c9e080315ee38f0763cca
siccaa <mahmoudyaya33@gmail.com>
2024-06-26 08:36:38 +0000

    he

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,19 +1,11 @@
-#include "main.h"
+#include <stdio.h>
 
-/**
- * main - Print _putchar
- * Return: Always 0
- */
-int main(void)
-{
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
+void main() {
+int c;
+
+c = getchar();
+while (c != EOF) {
+    putchar(c);
+    c = getchar();
+ }
 }
