6f5bd3bdaa9be475920676e43b2f6eecbb4633ef
siccaa <mahmoudyaya33@gmail.com>
2024-06-26 21:13:01 +0000

    hi

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,3 +1,9 @@
+#include <stdio.h>
+
+// Prototype for _isdigit
+int _isdigit(int c);
+
+// Function definition for _isdigit
 int _isdigit(int c)
 {
     if (c >= '0' && c <= '9')
@@ -8,4 +14,22 @@ int _isdigit(int c)
     {
         return 0;
     }
+}
+
+// Main function to test _isdigit
+int main(void)
+{
+    char test1 = '5';
+    char test2 = 'a';
+    char test3 = '0';
+    char test4 = '9';
+    char test5 = '!';
+
+    printf("Is %c a digit? %d\n", test1, _isdigit(test1)); // Expected output: 1
+    printf("Is %c a digit? %d\n", test2, _isdigit(test2)); // Expected output: 0
+    printf("Is %c a digit? %d\n", test3, _isdigit(test3)); // Expected output: 1
+    printf("Is %c a digit? %d\n", test4, _isdigit(test4)); // Expected output: 1
+    printf("Is %c a digit? %d\n", test5, _isdigit(test5)); // Expected output: 0
+
+    return 0;
 }
