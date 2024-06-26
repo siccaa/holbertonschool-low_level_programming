825fca890f80e452e6e1758d4bc70e28a2f4094f
siccaa <mahmoudyaya33@gmail.com>
2024-06-26 09:26:15 +0000

    main

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -7,17 +7,16 @@ void print_alphabet(void)
 {
     char alphabet[] = "abcdefghijklmnopqrstuvwxyz\n";
     for (int i = 0; i < 27; i++)
-    {
       
 }
 
 int main(void)
 {
-    print_alphabet();
-    return 0;
-}
+
+ 
 
 int _putchar(char c)
+  }
 {
     return putchar(c);
 }
