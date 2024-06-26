c89369917c09c6d1a64f6d10d9ebee3eb8634f26
siccaa <mahmoudyaya33@gmail.com>
2024-06-26 22:13:04 +0000

    hello

--- more_functions_nested_loops/0-main.c
+++ more_functions_nested_loops/0-main.c
@@ -5,17 +5,9 @@ int _isupper(int c);
 
 int main(void)
 {
-    char test1 = 'A';
-    char test2 = 'a';
-    char test3 = 'Z';
-    char test4 = 'z';
-    char test5 = '5';
+    char c = 'A';
 
-    printf("Is %c an uppercase letter? %d\n", test1, _isupper(test1)); /* Expected output: 1 */
-    printf("Is %c an uppercase letter? %d\n", test2, _isupper(test2)); /* Expected output: 0 */
-    printf("Is %c an uppercase letter? %d\n", test3, _isupper(test3)); /* Expected output: 1 */
-    printf("Is %c an uppercase letter? %d\n", test4, _isupper(test4)); /* Expected output: 0 */
-    printf("Is %c an uppercase letter? %d\n", test5, _isupper(test5)); /* Expected output: 0 */
+    printf("%c: %d\n", c, _isupper(c));
 
     return 0;
 }
