f9b588c66b7275873a47691ce16012950206f31b
siccaa <mahmoudyaya33@gmail.com>
2024-06-27 08:59:01 +0000

    hi

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 /**
  *
  * _isupper - check if c is uppercase
@@ -8,16 +8,14 @@
  * Return: 1 if c is upper, 0 if otherwise
  */
 
-int _isupper(int c);
-
+int _isupper(int c)
 {
     if (c >= 'A' && c <= 'Z')
-    {
+    
         return 1;
-    }
+    
     else
-    {
+    
         return 0;
-    }
+    
 }
- 
---
+++ more_functions_nested_loops/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_h_
+#define MAIN_H_
+
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+int _putchar(char c);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_dialgonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+
+#endif
